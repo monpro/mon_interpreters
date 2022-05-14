85e9af8bbbfd1dd8c9caa7d62278a663128047b1
monpro <monproshen@gmail.com>
2022-05-14 19:18:36 +1000

    set up baby step of interpreter - lox

---
+++ lox/src/main/java/Lox.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: lox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    final byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException {
+    final InputStreamReader input = new InputStreamReader(System.in);
+    final BufferedReader reader = new BufferedReader(input);
+
+    while (true){
+      System.out.println(">  ");
+      final String line = reader.readLine();
+      if (line == null) break;
+      run(line);;
+    }
+  }
+
+
+  // core logic of our interpreter
+  private static void run(String source) {
+    final Scanner scanner = new Scanner(source);
+    final List<Token> tokens = scanner.scanTokens();
+    // for now just print the tokens
+    for (final Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+}
---
+++ lox/src/main/java/Scanner.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Scanner {
+  private final String source;
+
+  public Scanner(String source) {
+    this.source = source;
+  }
+
+  public List<Token> scanTokens() {
+    return null;
+  }
+}
