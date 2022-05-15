132036115a718c6617b786e1f8fb218c0be56f75
monpro <monproshen@gmail.com>
2022-05-15 20:08:02 +1000

    add Token class and TokenType enum
    It will store the lexemes info as object for interpreter to use later

--- lox/src/main/java/Token.java
+++ lox/src/main/java/Token.java
@@ -1,2 +1,24 @@
 public class Token {
+
+  private final TokenType type;
+  private final String lexeme;
+  private final Object literal;
+  private final int line;
+
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  @Override
+  public String toString() {
+    return "Token{" +
+        "type=" + type +
+        ", lexeme='" + lexeme + '\'' +
+        ", literal=" + literal +
+        ", line=" + line +
+        '}';
+  }
 }
