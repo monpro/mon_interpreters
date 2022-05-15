1019a32e8b6603a33d198f3d787d285693391f1d
monpro <monproshen@gmail.com>
2022-05-15 20:30:52 +1000

    add several functions for scanner to generate one-character token and two characters operators

--- lox/src/main/java/Scanner.java
+++ lox/src/main/java/Scanner.java
@@ -1,13 +1,74 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scanner {
   private final String source;
+  private List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
 
   public Scanner(String source) {
     this.source = source;
   }
 
   public List<Token> scanTokens() {
-    return null;
+    while (!isAtEnd()) {
+      start = current;
+      scanToken();
+    }
+    tokens.add(new Token(TokenType.EOF, "", null, line));
+    return tokens;
   }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private void scanToken() {
+    final char c = advance();
+    switch (c) {
+      case '(': addToken(TokenType.LEFT_PAREN); break;
+      case ')': addToken(TokenType.RIGHT_PAREN); break;
+      case '{': addToken(TokenType.LEFT_BRACE); break;
+      case '}': addToken(TokenType.RIGHT_BRACE); break;
+      case ',': addToken(TokenType.COMMA); break;
+      case '.': addToken(TokenType.DOT); break;
+      case '-': addToken(TokenType.MINUS); break;
+      case '+': addToken(TokenType.PLUS); break;
+      case ';': addToken(TokenType.SEMICOLON); break;
+      case '*': addToken(TokenType.STAR); break;
+
+      case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+      case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+      case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+      case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+
+      default:
+        Lox.error(line, "unexpected character .");
+        break;
+    }
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    final String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    current += 1;
+    return true;
+  }
+
 }
