e519eaa20d9cb192df87d49e91bd3399178c4c2c
monpro <monproshen@gmail.com>
2022-05-20 15:18:23 +1000

    implement basic parser logic (unary, primary)
    TODO: add token consume and error report

--- lox/src/main/java/lox/Parser.java
+++ lox/src/main/java/lox/Parser.java
@@ -89,6 +89,31 @@ public class Parser {
   }
 
   private Expr unary() {
-    return null;
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+    return primary();
+  }
+
+  private Expr primary() {
+    if (match(FALSE)) return new Expr.Literal(false);
+    if (match(TRUE)) return new Expr.Literal(true);
+    if (match(NIL)) return new Expr.Literal(null);
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    if (match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ) after expression.");
+      return new Expr.Grouping(expr);
+    }
+    throw new RuntimeException("Expect expression");
+  }
+
+  private void consume(TokenType rightParen, String s) {
+
   }
 }
