331c4021893809e9cd0f1fae9ea8d8d7784137bc
monpro <monproshen@gmail.com>
2022-05-24 16:29:57 +1000

    add evaluating binary expression logic.

--- lox/src/main/java/lox/Interpreter.java
+++ lox/src/main/java/lox/Interpreter.java
@@ -4,6 +4,38 @@ public class Interpreter implements Expr.Visitor<Object>{
 
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
+    switch (expr.operator.type) {
+      case MINUS:
+        return (double)left - (double) right;
+      case SLASH:
+        return (double)left / (double) right;
+      case STAR:
+        return (double)left * (double) right;
+      case PLUS:
+        if (left instanceof Double && right instanceof Double) {
+          return (double) left + (double)right;
+        }
+        if (left instanceof String && right instanceof String) {
+          return left + (String)right;
+        }
+        break;
+      case GREATER:
+        return (double)left > (double)right;
+      case GREATER_EQUAL:
+        return (double)left >= (double) right;
+      case LESS:
+        return (double)left < (double) right;
+      case LESS_EQUAL:
+        return (double)left <= (double)right;
+      case BANG_EQUAL:
+        return !isEqual(left, right);
+      case EQUAL_EQUAL:
+        return isEqual(left, right);
+      default:
+        break;
+    }
     return null;
   }
 
@@ -33,7 +65,7 @@ public class Interpreter implements Expr.Visitor<Object>{
   }
 
   /**
-   * return false if object is null or false
+   * return false if the object is null or false
    * @param object Object.
    * @return boolean
    */
@@ -41,6 +73,18 @@ public class Interpreter implements Expr.Visitor<Object>{
     if (object == null) return false;
     if (object instanceof Boolean) return (boolean) object;
     return true;
+  }
+
+  /**
+   * return true if two object is equal, false otherwise
+   * @param object1 Object.
+   * @param object2 Object.
+   * @return Boolean.
+   */
+  private boolean isEqual(Object object1, Object object2) {
+    if (object1 == null && object2 == null) return true;
+    if (object1 == null) return false;
+    return object1.equals(object2);
   }
 
   private Object evaluate(Expr expr) {
