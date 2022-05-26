549eaf1dc6899209aa43bdf3bd03d0a467f1ff22
monpro <monproshen@gmail.com>
2022-05-26 20:24:53 +1000

    add var declaration logic in parser and synchronize error parsing logic

--- lox/src/main/java/lox/Parser.java
+++ lox/src/main/java/lox/Parser.java
@@ -16,15 +16,55 @@ public class Parser {
   public List<Statement> parse() {
     List<Statement> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
     return statements;
+  }
+
+  private Statement declaration() {
+    try {
+      if (match(VAR)) return varDeclaration();
+      else return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
+  private Statement varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name");
+    Expr init = null;
+
+    if (match(EQUAL)) {
+      init = expression();
+    }
+    consume(SEMICOLON, "Expect ; after variable declaration");
+    return new Statement.Var(name, init);
   }
 
   private Statement statement() {
     // TODO: fill in more statement types later
     if (match(PRINT)) return printStateStatement();
     else return expressionStatement();
+  }
+
+  /**
+   * once we met a error say passing too many arguments into function.
+   * we should not just break the interpreter
+   * instead we should skip those `invalid` statement and go to the next valid one.
+   */
+  private void synchronize() {
+    advance();
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS: case FOR: case FUN: case IF: case PRINT:
+        case RETURN: case VAR: case WHILE:
+          return;
+      }
+      advance();
+    }
   }
 
   private Statement printStateStatement() {
@@ -131,7 +171,9 @@ public class Parser {
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
     }
-
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
+    }
     if (match(LEFT_PAREN)) {
       Expr expr = expression();
       consume(RIGHT_PAREN, "Expect ) after expression.");
