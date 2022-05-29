a35dc4b188e0a0bf952f0e47233f0d623a1fb898
monpro <monproshen@gmail.com>
2022-05-29 22:47:12 +1000

    add enclosing logic in support to variable scoping

--- lox/src/main/java/lox/Environment.java
+++ lox/src/main/java/lox/Environment.java
@@ -6,6 +6,15 @@ import java.util.Map;
 public class Environment {
 
   private final Map<String, Object> values = new HashMap<>();
+  private final Environment enclosing;
+
+  public Environment() {
+    this.enclosing = null;
+  }
+
+  public Environment(Environment enclosing) {
+    this.enclosing = enclosing;
+  }
 
   public void define(String name, Object value) {
     values.put(name, value);
@@ -15,12 +24,19 @@ public class Environment {
     if (values.containsKey(name.lexeme)) {
       return values.get(name.lexeme);
     }
+    if (enclosing != null) {
+      return enclosing.get(name);
+    }
     throw new RunTimeError(name, "Undefined Variable: '" + name.lexeme + "' .");
   }
 
   public void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)) {
       values.put(name.lexeme, value);
+      return;
+    }
+    if (enclosing != null) {
+      enclosing.assign(name, value);
       return;
     }
     throw new RunTimeError(name, "Undefined variable '" + name.lexeme + "'.");
