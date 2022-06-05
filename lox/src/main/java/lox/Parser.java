b51b109655eecdd1cac32c4d7482a68a48e0c6ae
monpro <monproshen@gmail.com>
2022-06-05 21:20:49 +1000

    add parser logic to generate Function Statement

--- lox/src/main/java/lox/Parser.java
+++ lox/src/main/java/lox/Parser.java
@@ -24,6 +24,7 @@ public class Parser {
 
   private Statement declaration() {
     try {
+      if (match(FUN)) return function("function");
       if (match(VAR)) return varDeclaration();
       else return statement();
     } catch (ParseError error) {
@@ -31,6 +32,27 @@ public class Parser {
       return null;
     }
   }
+
+  // function rule, will be reused later for classes method
+  // currently only have function type
+  private Statement function(String type) {
+    Token name = consume(IDENTIFIER, "Expect " + type + " name");
+    consume(LEFT_PAREN, "Expect '(' after " + type + " name.");
+    List<Token> parameters = new ArrayList<>();
+    if (!check(RIGHT_PAREN)) {
+      do {
+        if (parameters.size() >= 255) {
+          error(peek(), "Cannot have more than 255 parameters");
+        }
+        parameters.add(consume(IDENTIFIER, "Expect parameter name."));
+      } while (match(COMMA));
+    }
+    consume(RIGHT_PAREN, "Expect ')' after parameters.");
+    consume(LEFT_BRACE, "Expect '{' before " + type + " body.");
+    List<Statement> body = block();
+    return new Statement.Function(name, parameters, body);
+  }
+
 
   private Statement varDeclaration() {
     Token name = consume(IDENTIFIER, "Expect variable name");
