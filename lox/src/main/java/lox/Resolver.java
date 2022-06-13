56e4aa628c83295035881ef69542b0e13fd77ee5
monpro <monproshen@gmail.com>
2022-06-13 10:34:25 +1000

    add expression resolved logic at Resolver

--- lox/src/main/java/lox/Resolver.java
+++ lox/src/main/java/lox/Resolver.java
@@ -38,16 +38,23 @@ public class Resolver implements Expr.Visitor<Void>, Statement.Visitor<Void> {
 
   @Override
   public Void visitBinaryExpr(Expr.Binary expr) {
+    resolve(expr.left);
+    resolve(expr.right);
     return null;
   }
 
   @Override
   public Void visitCallExpr(Expr.Call expr) {
+    resolve(expr.callee);
+    for (Expr argument : expr.arguments) {
+      resolve(argument);
+    }
     return null;
   }
 
   @Override
   public Void visitGroupingExpr(Expr.Grouping expr) {
+    resolve(expr.expression);
     return null;
   }
 
@@ -58,6 +65,7 @@ public class Resolver implements Expr.Visitor<Void>, Statement.Visitor<Void> {
 
   @Override
   public Void visitUnaryExpr(Expr.Unary expr) {
+    resolve(expr.right);
     return null;
   }
 
@@ -93,6 +101,8 @@ public class Resolver implements Expr.Visitor<Void>, Statement.Visitor<Void> {
 
   @Override
   public Void visitLogicalExpr(Expr.Logical expr) {
+    resolve(expr.left);
+    resolve(expr.right);
     return null;
   }
 
